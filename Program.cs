bca746bad0d61cbdd0235cc275d0f108dc171d0e
Gabriel <gaplogan@gmail.com>
2021-02-08 17:31:12 -0300

    2021.02.08.17.31

--- Program.cs
+++ Program.cs
@@ -6,8 +6,10 @@ namespace cs_1._18_AreaPerimRetang
     {
         static void Main(string[] args)
         {
+            // Declaração de variáveis do programa
             double lado1, lado2, perim, area;
 
+            // {Entrada de dados}
             Console.Write("Digite o valor do primeiro lado: ");
             lado1 = Math.Abs(double.Parse(Console.ReadLine())); //garante Lado1 com valor positivo
 
@@ -16,9 +18,11 @@ namespace cs_1._18_AreaPerimRetang
 
             Console.WriteLine(); // Salta uma linha
 
+            // {Processamento de dados}
             perim = 2 * lado1 + 2 * lado2; //fórmula do perímetro do retângulo
             area = lado1 * lado2;
 
+            // {Saída de dados}
             Console.WriteLine($"Perímetro: {perim}");
             Console.WriteLine($"Área.....: {area}");
         }
